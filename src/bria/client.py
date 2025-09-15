f92edd18e4a999e4365b0617f44f5dd36d59d420
galaviv938 <galaviv938@gmail.com>
2025-09-15 14:14:34 +0300

    remove main for tests

--- src/bria/client.py
+++ src/bria/client.py
@@ -9,12 +9,3 @@ class Bria:
         self.api_token = api_token
         self.edit = Edit(api_token)
         self.status = StatusService(api_token)
-
-if __name__ == '__main__':
-    import os
-    API_TOKEN = os.getenv("BRIA_API_TOKEN")
-
-    b = Bria('d3fc93bca79948d2ac1c01e046e35753')
-    input_image_url = "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1279726757-651427fccdc51.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*"
-    x = b.edit.remove_background(input_image_url)
-    y= 0
